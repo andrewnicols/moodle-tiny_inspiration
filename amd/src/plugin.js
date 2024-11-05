8352317bccf8d9530645bb2bbea56cb0cb098004
Andrew Nicols <andrew@nicols.co.uk>
2024-11-05 15:39:32 +0800

    Initial plugin configuration

---
+++ amd/src/plugin.js
@@ -0,0 +1,43 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tiny Inspirational Quotes plugin.
+ *
+ * @module     tiny_inspiration/plugin
+ * @copyright  Andrew Lyons <andrew@nicols.co.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+import {getTinyMCE} from 'editor_tiny/loader';
+
+const pluginName = 'tiny_inspiration/plugin';
+
+export default new Promise((resolve) => {
+    // Initialise the plugin.
+    Promise.all([
+        getTinyMCE(),
+    ]).then(([tinyMCE]) => {
+        // This is where we define icons, buttons, menu items, and so on.
+        tinyMCE.PluginManager.add(pluginName, () => {
+            // Note: The contents of this method must be synchronous and not return any Promise.
+
+            return;
+        });
+
+        // Resolve the Plugin and include configuration.
+        resolve([pluginName, () => {}]);
+    });
+});
